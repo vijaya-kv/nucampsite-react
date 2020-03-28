68aa202a73715441c2d420ee63652d5114ab8af4
vijaya-kv <60544662+vijaya-kv@users.noreply.github.com>
2020-03-28 00:05:49 -0500

    Functional Components

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,34 +1,43 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Directory from './DirectoryComponent';
-import CampsiteInfoComponent from './CampsiteInfoComponent';
-import { CAMPSITES } from '../shared/campsites';
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Directory from "./DirectoryComponent";
+import CampsiteInfoComponent from "./CampsiteInfoComponent";
+import { CAMPSITES } from "../shared/campsites";
 
 class Main extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
-      campsites : CAMPSITES,
+      campsites: CAMPSITES,
       selectedCampsite: null
     };
   }
 
   onCampsiteSelect(campsiteId) {
-    this.setState({selectedCampsite : campsiteId});
-}
+    this.setState({ selectedCampsite: campsiteId });
+  }
 
   render() {
-      return (
-          <div>
-              <Navbar dark color="primary">
-              <div className="container">
-                  <NavbarBrand href="/">NuCamp</NavbarBrand>
-              </div>
-              </Navbar>
-              <Directory campsites={ this.state.campsites } onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
-              <CampsiteInfoComponent campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}></CampsiteInfoComponent>
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">NuCamp</NavbarBrand>
           </div>
-      );
+        </Navbar>
+        <Directory
+          campsites={this.state.campsites}
+          onClick={campsiteId => this.onCampsiteSelect(campsiteId)}
+        />
+        <CampsiteInfoComponent
+          campsite={
+            this.state.campsites.filter(
+              campsite => campsite.id === this.state.selectedCampsite
+            )[0]
+          }
+        ></CampsiteInfoComponent>
+      </div>
+    );
   }
 }
 
