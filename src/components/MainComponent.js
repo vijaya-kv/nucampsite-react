cfe572cef5b21017e0d66514a56735b7a324f3cc
vijaya-kv <60544662+vijaya-kv@users.noreply.github.com>
2020-03-29 20:41:25 -0500

    React Breadcrumbs

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import { CAMPSITES } from "../shared/campsites";
 import { COMMENTS } from "../shared/comments";
 import { PARTNERS } from "../shared/partners";
 import { PROMOTIONS } from "../shared/promotions";
+import CampsiteInfo from "./CampsiteInfoComponent";
 
 class Main extends Component {
   constructor(props) {
@@ -30,12 +31,20 @@ class Main extends Component {
               />
           );
       }
+
+    const CampsiteWithId = ({match}) => {
+        return (
+            <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                          comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} />
+        )
+    };
     return (
       <div>
         <Header/>
         <Switch> 
             <Route path="/home" component={ HomePage } />
             <Route exact path="/directory" render={() => <Directory campsites={this.state.campsites} />} />
+            <Route exact path="/directory/:campsiteId" component={CampsiteWithId} />
             <Route path="/contactus" component={ Contact } />
             <Redirect to="/home" />
         </Switch>
