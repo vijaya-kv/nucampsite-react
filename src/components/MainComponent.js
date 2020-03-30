756549c2a8f3e2685be5a6664beb3c518588d79d
vijaya-kv <60544662+vijaya-kv@users.noreply.github.com>
2020-03-29 19:39:07 -0500

    Adding to the Single Page App

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,21 +3,31 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from "./HeaderComponent";
 import Home from "./HomeComponent";
 import Directory from "./DirectoryComponent";
+import Contact from './ContactComponent';
 import Footer from "./FooterComponent";
 import { CAMPSITES } from "../shared/campsites";
+import { COMMENTS } from "../shared/comments";
+import { PARTNERS } from "../shared/partners";
+import { PROMOTIONS } from "../shared/promotions";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      campsites: CAMPSITES
+      campsites: CAMPSITES,
+      comments: COMMENTS,
+      partners: PARTNERS,
+      promotions: PROMOTIONS
     };
   }
 
   render() {
       const HomePage = () => {
           return (
-              <Home />
+              <Home  campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                     promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                     partner={this.state.partners.filter(partner => partner.featured)[0]}
+              />
           );
       }
     return (
@@ -26,6 +36,7 @@ class Main extends Component {
         <Switch> 
             <Route path="/home" component={ HomePage } />
             <Route exact path="/directory" render={() => <Directory campsites={this.state.campsites} />} />
+            <Route path="/contactus" component={ Contact } />
             <Redirect to="/home" />
         </Switch>
         <Footer/>
