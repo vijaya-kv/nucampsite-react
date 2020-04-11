8fc01766473709bebb6c2312de1fa1756b79d2a4
vijaya-kv <60544662+vijaya-kv@users.noreply.github.com>
2020-04-10 22:57:50 -0500

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -33,8 +33,7 @@ class CommentForm extends Component {
   }
 
   handleSubmit(values) {
-    console.log("Current state is :" + JSON.stringify(values));
-    alert("Current state is :" + JSON.stringify(values));
+    this.props.addComment (this.props.campsiteId, values.rating, values.author, values.text);
   }
 
 
@@ -116,7 +115,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId}) {
   if (comments && comments != null) {
     return (
       <div className="col-md-5 m-1">
@@ -127,7 +126,10 @@ function RenderComments({ comments }) {
             <p>-- {comment.author},{" "} {new Intl.DateTimeFormat("en-US", { year: "numeric", month: "short", day: "2-digit"}).format(new Date(Date.parse(comment.date)))}</p>
           </div>
         ))}
-        <CommentForm />
+        <CommentForm 
+            campsiteId = {campsiteId}
+            addComment = {addComment}
+        />
       </div>
     );
   }
@@ -152,7 +154,11 @@ function CampsiteInfo(props) {
         </div>
         <div className="row">
           <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.comments} />
+          <RenderComments 
+              comments={props.comments} 
+              addComment={props.addComment}
+              campsiteId={props.campsite.id}
+          />
         </div>
       </div>
     );
--- src/redux/campsites.js
+++ src/redux/campsites.js
@@ -1,4 +1,5 @@
 import { CAMPSITES } from '../shared/campsites';
+import { ADD_COMMENT } from './ActionTypes';
 
 export const Campsites = (state = CAMPSITES, action) => {
     switch (action.type)  {
